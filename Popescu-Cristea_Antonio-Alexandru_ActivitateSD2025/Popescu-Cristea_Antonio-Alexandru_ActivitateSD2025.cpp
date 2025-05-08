e719b80cfc32c0f6d6d6e75975bb95ceaa4e655c
Popescu-Cristea Antonio-Alexandru <60746739+antoniotony04@users.noreply.github.com>
2025-05-08 15:07:54 +0200

    Popescu-Cristea_Antonio-Alexandru_ActivitateSD2025.cpp

--- Popescu-Cristea_Antonio-Alexandru_ActivitateSD2025/Popescu-Cristea_Antonio-Alexandru_ActivitateSD2025.cpp
+++ Popescu-Cristea_Antonio-Alexandru_ActivitateSD2025/Popescu-Cristea_Antonio-Alexandru_ActivitateSD2025.cpp
@@ -1,119 +1,48 @@
-#include <stdio.h>
-#include <queue>
-#include <string>
-#include <iostream>
+/*
+    Cerinta:
+    Scrie un program C++ care defineste o structura Student cu urmatoarele campuri:
+        nume (sir de caractere),
+        varsta (int),
+        medie (float).
+        Programul trebuie sa:
+        Citeasca datele a n studenti de la tastatura.
+        Stocheze datele intr-un vector de structuri.
+        Afiseze studentii care au media mai mare sau egala cu 8.00.
+*/
 
+#include <iostream>
+#include <string>
 using namespace std;
 
-
-//verificare graf
-template<typename TnodeInfo, typename TedgeInfo>
-class Graph {
-public:
-    int N;
-    char **A;
-    TnodeInfo *nodeInfo;
-    TedgeInfo **edgeInfo;
-
-    Graph(int numNodes) {
-        int i, j;
-
-        N = numNodes;
-
-        A = new char*[N];
-        for (i = 0; i < N; i++)
-            A[i] = new char[N];
-
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                A[i][j] = 0;
-
-        nodeInfo = new TnodeInfo[N];
-
-        edgeInfo = new TedgeInfo*[N];
-        for (i = 0; i < N; i++)
-            edgeInfo[i] = new TedgeInfo[N];
-    }
-
-    void setNodeInfo(int i, TnodeInfo info) {
-        nodeInfo[i] = info;
-    }
-
-    TnodeInfo getNodeInfo(int i) {
-        return nodeInfo[i];
-    }
-
-    void addEdge(int i, int j) {
-        A[i][j] = A[j][i] = 1;
-    }
-    void removeEdge(int i, int j) {
-        A[i][j] = A[j][i] = 0; }
-
-    void setEdgeInfo(int i, int j, TedgeInfo info) {
-        edgeInfo[i][j] = edgeInfo[j][i] = info; }
-
-    TedgeInfo getEdgeInfo(int i, int j) {
-        return edgeInfo[i][j]; }
-
-    ~Graph() {
-        int i;
-        for (i = 0; i < N; i++) {
-            delete A[i];
-            delete edgeInfo[i];
-        }
-        delete A;
-        delete edgeInfo;
-        delete nodeInfo;
-    }
+struct Student {
+    string nume;
+    int varsta;
+    float medie;
 };
 
-Graph<char*, int> g(12);
-char* visited;
-
-void dfs(int x) {
-    int y;
-    printf("%d\n", x);
-    visited[x] = 1;
-
-    for (y = 0; y < g.N; y++)
-        if (g.A[x][y] && !visited[y])
-            dfs(y);
-}
-
-void bfs(int S) {
-    std::queue<int> Q;
-    int x, y;
-
-    Q.push(S);
-    visited[S] = 1;
-
-    while (!Q.empty()) {
-        x = Q.front();
-        Q.pop();
-        printf("%d\n", x);
-        for (y = 0; y < g.N; y++)
-            if (g.A[x][y] && !visited[y]) {
-                visited[y] = 1;
-                Q.push(y);
-            }
-    }
-}
-
-
-
 int main() {
-    int i;
-    g.addEdge(0, 4);    g.addEdge(2, 4);
-    g.addEdge(4, 6); g.addEdge(7,4);
-    g.addEdge(6,5); g.addEdge(7,3);
-    g.addEdge(3,1);
+    int n;
+    cout << "Introdu numarul de studenti: ";
+    cin >> n;
 
+    Student studenti[100];
 
-    visited = new char[g.N];
-    for (i = 0; i < g.N; i++)
-        visited[i] = 0;
+    for (int i = 0; i < n; i++) {
+        cout << "Student " << i + 1 << ":\n";
+        cout << "Nume: ";
+        cin >> studenti[i].nume;
+        cout << "Varsta: ";
+        cin >> studenti[i].varsta;
+        cout << "Medie: ";
+        cin >> studenti[i].medie;
+    }
 
-    dfs(4);
+    cout << "\nStudenti cu media >= 8.00:\n";
+    for (int i = 0; i < n; i++) {
+        if (studenti[i].medie >= 8.0) {
+            cout << studenti[i].nume << ", " << studenti[i].varsta << " ani, Media: " << studenti[i].medie << endl;
+        }
+    }
+
     return 0;
 }
-
